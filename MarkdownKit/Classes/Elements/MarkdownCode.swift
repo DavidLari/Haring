de079a5953d0b5bef355aaf3a8ab6a61fdef9f25
David Lari <david@davidlari.com>
2017-03-06 10:04:18 -0500

    Code parsing fixes from @yoha

--- MarkdownKit/Classes/Elements/MarkdownCode.swift
+++ MarkdownKit/Classes/Elements/MarkdownCode.swift
@@ -10,7 +10,7 @@ import UIKit
 
 open class MarkdownCode: MarkdownCommonElement {
 
-  fileprivate static let regex = "(\\s+|^)(`+)(\\s*.*?[^`]\\s*)(\\1)(?!`)"
+  fileprivate static let regex = "(\\s+|^)(\\`+)(.+?)(\\2)"
 
   open var font: UIFont?
   open var color: UIColor?
@@ -28,6 +28,12 @@ open class MarkdownCode: MarkdownCommonElement {
     let matchString: String = attributedString.attributedSubstring(from: range).string
     guard let unescapedString = matchString.unescapeUTF16() else { return }
     attributedString.replaceCharacters(in: range, with: unescapedString)
-    attributedString.addAttributes(attributes, range: NSRange(location: range.location, length: unescapedString.characters.count))
+  
+    var amendedAttributes = attributes
+    amendedAttributes[NSBackgroundColorAttributeName] = UIColor(red: 0.91, green: 0.91, blue: 0.91, alpha: 1.0)
+    amendedAttributes[NSForegroundColorAttributeName] = UIColor(red: 0.95, green: 0.25, blue: 0.44, alpha: 1.0)
+    amendedAttributes[NSFontAttributeName] = UIFont(name: "Menlo-Regular", size: 16)
+  
+    attributedString.addAttributes(amendedAttributes, range: NSRange(location: range.location, length: unescapedString.characters.count))
   }
 }
